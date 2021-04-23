4b6849ebe45380273b84a2c525874763f589abff
Omar Branez <omar.branez@gmail.com>
2021-04-23 18:56:18 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
+require 'sinatra/base'
 class Helpers
+
+    def self.current_user(session)
+    #class methods 
+       @user = User.find_by_id(session[:user_id])
+       #for session, it's user_id not just id
+    end
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+        # double bang !! is a boolean
+    end
+
 end
